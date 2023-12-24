edea28b55a69a152039c5dd604a98b2b372261bf
Abdullah Buzkan <abdullahbuzkan2023@gmail.com>
2023-12-25 00:22:05 +0300

    proje katmanlar eklendi ef paketleri indirildi

---
+++ EntityLayer/Concrete/ServiceHousing.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class ServiceHousing
+    {
+        public int ServiceHousingID { get; set; }
+        public string Baslik { get; set; }
+        public string Gorsel { get; set; }
+        public int Fiyat { get; set; }
+        public string TapuDurumu { get; set; }
+        public string YapininDurumu { get; set; }
+        public string Isitma { get; set; }
+        public int BinaKatSayisi { get; set; }
+        public string YapininCephesi { get; set; }
+        public string YapininSekli { get; set; }
+        public int TuvaletSayisi { get; set; }
+        public int BalkonSayisi { get; set; }
+        public int BanyoSayisi { get; set; }
+        public int SalonSayisi { get; set; }
+        public int OdaSayisi { get; set; }
+        public int KiraGetirisi { get; set; }
+        public bool TakasYapilir { get; set; }
+        public string KullanimDurumu { get; set; }
+        public string YakitTipi { get; set; }
+        public int BulunduguKat { get; set; }
+        public int Aidat { get; set; }
+        public bool KrediyeUygun { get; set; }
+        public bool Devren { get; set; }
+        public string UzunAciklama { get; set; }
+        public string KonumLink { get; set; }
+        public int KonumId { get; set; }
+        public int BilgiId { get; set; }
+        [ForeignKey("KonumId")]
+        public virtual Konum Konum { get; set; }
+        [ForeignKey("BilgiId")]
+        public virtual Bilgi Bilgi { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/ServiceTerrain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class ServiceTerrain
+    {
+        public int ServiceTerrainID { get; set; }
+        public string Baslik { get; set; }
+        public string Gorsel { get; set; }
+        public int Fiyat { get; set; }
+        public string TapuDurumu { get; set; }
+        public bool KatKarsiligi { get; set; }
+        public bool KrediyeUygun { get; set; }
+        public int TapuParsel { get; set; }
+        public int TapuPafta { get; set; }
+        public int TapuAda { get; set; }
+        public int VadeVarmi { get; set; }
+        public int IzinVerilenKatAdedi { get; set; }
+        public string BelediyeAdi { get; set; }
+        public bool TakasYapilir { get; set; }
+        public bool IskanDurumu { get; set; }
+        public int TabanInsaaAlani { get; set; }
+        public int ToplamInsaatAlani { get; set; }
+        public int MaksimumBinaYuksekligi { get; set; }
+        public string UzunAciklama { get; set; }
+        public string KonumLink { get; set; }
+        public int KonumId { get; set; }
+        public int BilgiId { get; set; }
+        [ForeignKey("KonumId")]
+        public virtual Konum Konum { get; set; }
+        [ForeignKey("BilgiId")]
+        public virtual Bilgi Bilgi { get; set; }
+    }
+}
