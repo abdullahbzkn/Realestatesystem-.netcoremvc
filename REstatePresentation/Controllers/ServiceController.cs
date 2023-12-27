49200952320ef43de723771c45174552111c2885
Abdullah Buzkan <abdullahbuzkan2023@gmail.com>
2023-12-27 17:49:15 +0300

    testcontroller çalışmıyor 91

---
+++ REstatePresentation/Controllers/ServiceController.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using REstatePresentation.Models;
+
+namespace REstatePresentation.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IServiceHousingService _serviceHousingService;
+
+        public ServiceController(IServiceHousingService serviceHousingService)
+        {
+            _serviceHousingService = serviceHousingService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _serviceHousingService.GetListAll();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddServiceHousing()
+        {
+            return View(new ServiceHousingAddViewModel());
+        }
+        [HttpPost]
+        public IActionResult AddServiceHousing(ServiceHousingAddViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                _serviceHousingService.Insert(new ServiceHousing()
+                {
+                    Baslik = model.Baslik,
+                    Fiyat = model.Fiyat,
+                    UzunAciklama = model.UzunAciklama
+                });
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ REstatePresentation/Models/ServiceHousingAddViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace REstatePresentation.Models
+{
+    public class ServiceHousingAddViewModel
+    {
+        [Display(Name = "Başlık")]
+        [Required(ErrorMessage = "Başlık boş geçilemez")]
+        public string Baslik { get; set; }
+
+        [Display(Name = "Fiyat")]
+        [Required(ErrorMessage = "Fiyat boş geçilemez")]
+        public int Fiyat { get; set; }
+
+        [Display(Name = "Uzun Açıklama")]
+        [Required(ErrorMessage = "Açıklama boş geçilemez")]
+        public string UzunAciklama { get; set; }
+
+    }
+}
