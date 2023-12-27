71615e0400a37a403c80d5fb81811ad7b4d81d23
Abdullah Buzkan <abdullahbuzkan2023@gmail.com>
2023-12-27 18:20:58 +0300

    tek fark defaulttaki alındı 94

--- REstatePresentation/Controllers/ServiceHousingController.cs
+++ REstatePresentation/Controllers/ServiceHousingController.cs
@@ -4,21 +4,25 @@ using DataAccessLayer.Concrete.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using REstatePresentation.Models;
+using System.ComponentModel;
 
 namespace REstatePresentation.Controllers
 {
-    public class ServiceController : Controller
+    public class ServiceHousingController : Controller
     {
-        private readonly IServiceHousingService _serviceHousingService;
+        //private readonly IServiceHousingService _serviceHousingService;
+        ServiceHousingManager serviceHousingManager = new ServiceHousingManager(new EfServiceHousingDal());
 
-        public ServiceController(IServiceHousingService serviceHousingService)
-        {
-            _serviceHousingService = serviceHousingService;
-        }
+
+        //public ServiceController(IServiceHousingService serviceHousingService)
+        //{
+        //    _serviceHousingService = serviceHousingService;
+        //}
 
         public IActionResult Index()
         {
-            var values = _serviceHousingService.GetListAll();
+            //var values = _serviceHousingService.GetListAll();
+            var values = serviceHousingManager.GetListAll();
             return View(values);
         }
         [HttpGet]
@@ -31,7 +35,8 @@ namespace REstatePresentation.Controllers
         {
             if(ModelState.IsValid)
             {
-                _serviceHousingService.Insert(new ServiceHousing()
+                //_serviceHousingService.Insert(new ServiceHousing()
+                serviceHousingManager.Insert(new ServiceHousing()
                 {
                     Baslik = model.Baslik,
                     Fiyat = model.Fiyat,
@@ -40,6 +45,13 @@ namespace REstatePresentation.Controllers
                 return RedirectToAction("Index");
             }
             return View(model);
+        }
+
+        public IActionResult DeleteServiceHousing(int id)
+        {
+            var values = serviceHousingManager.GetById(id);
+            serviceHousingManager.Delete(values);
+            return RedirectToAction("Index");
         }
     }
 }
