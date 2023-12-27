501a0ca6918962a89bfc7a45759dfd1a9c9bc839
Abdullah Buzkan <abdullahbuzkan2023@gmail.com>
2023-12-27 16:52:01 +0300

    testcontroller çalışmıyor 91

---
+++ REstatePresentation/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace REstatePresentation.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly IServiceHousingService _serviceHousingService;
+
+        public TestController(IServiceHousingService serviceHousingService)
+        {
+            _serviceHousingService = serviceHousingService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _serviceHousingService.GetListAll();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddServiceHousing()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddServiceHousing(ServiceHousing serviceHousing)
+        {
+            _serviceHousingService.Insert(serviceHousing);
+            return RedirectToAction("Index");
+        }
+    }
+}
