3fa73edfafebb33f662ec3a0e52670d8fe0d520a
Abdullah Buzkan <abdullahbuzkan2023@gmail.com>
2024-01-02 00:16:38 +0300

    user kısmına başlandı viewcomponente giriş yapıldı ilanlar ana sayfada görüntülenebilir halde

--- REstatePresentation/Controllers/DefaultController.cs
+++ REstatePresentation/Controllers/DefaultController.cs
@@ -8,18 +8,9 @@ namespace REstatePresentation.Controllers
 {
     public class DefaultController : Controller
     {
-        private readonly IServiceHousingService _serviceHousingService;
-        public DefaultController(IServiceHousingService serviceHousingService)
-        {
-            _serviceHousingService = serviceHousingService;
-        }
-        //ServiceHousingManager serviceHousingManager = new ServiceHousingManager(new EfServiceHousingDal(), new EfServicePhotoDal(), new REstateContext());
         public IActionResult Index()
         {
-            //var values = serviceHousingManager.GetListAll();
-            var values = _serviceHousingService.GetListAll();
-            return View(values);
-            //return View();
+            return View();
         }
     }
 }
---
+++ REstatePresentation/ViewComponents/_GalleryPartial.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace REstatePresentation.ViewComponents
+{
+    public class _GalleryPartial: ViewComponent
+    {
+        private readonly IServiceHousingService _serviceHousingService;
+
+        public _GalleryPartial(IServiceHousingService serviceHousingService)
+        {
+            _serviceHousingService = serviceHousingService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _serviceHousingService.GetListAll();
+            return View(values);
+        }
+    }
+}
