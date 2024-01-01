453d1211de766f649dd4ae5a105b71b899293047
Abdullah Buzkan <abdullahbuzkan2023@gmail.com>
2024-01-01 18:25:55 +0300

    team için validation rules eklendi

---
+++ BusinessLayer/ValidationRules/TeamValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class TeamValidator : AbstractValidator<Team>
+    {
+        public TeamValidator()
+        {
+            RuleFor(x => x.KisiAdi).NotEmpty().WithMessage("Takım Arkadaşı İsim Bilgileri Boş Geçilemez");
+            RuleFor(x => x.KisiAdi).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakter veri girişi yapın");
+            RuleFor(x => x.Aciklama).MinimumLength(8).WithMessage("Lütfen en az 8 karakter veri girişi yapın");
+            RuleFor(x => x.Aciklama).NotEmpty().WithMessage("Açıklama Bilgisi Boş Geçilemez");
+            RuleFor(x => x.FotografYolu).NotEmpty().WithMessage("Fotoğraf Yolu Boş Geçilemez");
+        }
+    }
+}
