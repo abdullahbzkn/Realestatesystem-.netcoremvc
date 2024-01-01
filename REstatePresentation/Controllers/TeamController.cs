169e52d16c120e6deaf23e1ed68b60f0daff262a
Abdullah Buzkan <abdullahbuzkan2023@gmail.com>
2024-01-01 18:14:30 +0300

    team kısmı controller ve viewlar ekleniyor

---
+++ REstatePresentation/Controllers/TeamController.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace REstatePresentation.Controllers
+{
+    public class TeamController : Controller
+    {
+        private readonly ITeamService _teamService;
+
+        public TeamController(ITeamService teamService)
+        {
+            _teamService = teamService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _teamService.GetListAll();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddTeam()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddTeam(Team team)
+        {
+            _teamService.Insert(team);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditTeam()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult EditTeam(Team team)
+        {
+            _teamService.Update(team);
+            return RedirectToAction("Index");
+        }
+    }
+}
