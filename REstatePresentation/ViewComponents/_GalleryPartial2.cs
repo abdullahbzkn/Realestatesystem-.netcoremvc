76e781712ffb77e21ec1732defc6adf0ee92f4a8
Abdullah Buzkan <abdullahbuzkan2023@gmail.com>
2024-01-04 18:16:19 +0300

    gallerypartial2 eklendi

---
+++ REstatePresentation/ViewComponents/_GalleryPartial2.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace REstatePresentation.ViewComponents
+{
+    public class _GalleryPartial2: ViewComponent
+    {
+        private readonly IServiceTerrainService _serviceTerrainService;
+        private readonly REstateContext _context;
+        public _GalleryPartial2(IServiceTerrainService serviceTerrainService,REstateContext rEstateContext)
+        {
+            _serviceTerrainService = serviceTerrainService;
+            _context = rEstateContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.ServiceTerrains.Include(c => c.ServiceInfo).Include(c => c.ServicePhotos).Include(c => c.ServiceMap).Where(r => r.Status == true).ToList());
+        }
+    }
+}
