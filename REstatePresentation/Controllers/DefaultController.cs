4597150b208f06c9f8c5b2e7e541c8ddf4782536
Abdullah Buzkan <abdullahbuzkan2023@gmail.com>
2024-01-05 19:09:11 +0300

    ilan detay sayfasına başlandı layout oluşturuldu

--- REstatePresentation/Controllers/DefaultController.cs
+++ REstatePresentation/Controllers/DefaultController.cs
@@ -6,17 +6,33 @@ using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using REstatePresentation.Models;
 
 namespace REstatePresentation.Controllers
 {
     [AllowAnonymous]
     public class DefaultController : Controller
     {
+        private readonly IServiceMapService _serviceMapService;
+        private readonly IServiceInfoService _serviceInfoService;
+        private readonly IServiceHousingService _serviceHousingService;
+        private readonly IServicePhotoService _servicePhotoService;
+        private ServiceInfo newServiceInfo;
         private readonly IContactService _contactService;
         private readonly REstateContext _context;
 
-        public DefaultController(IContactService contactService, REstateContext context)
+        public DefaultController(
+            IServiceMapService serviceMapService,
+            IServiceInfoService serviceInfoService,
+            IServiceHousingService serviceHousingService,
+            IServicePhotoService servicePhotoService,
+            IContactService contactService,
+            REstateContext context)
         {
+            _serviceMapService = serviceMapService;
+            _serviceInfoService = serviceInfoService;
+            _serviceHousingService = serviceHousingService;
+            _servicePhotoService = servicePhotoService;
             _contactService = contactService;
             _context = context;
         }
@@ -39,6 +55,39 @@ namespace REstatePresentation.Controllers
 
             return View();
         }
+
+
+        public IActionResult ServiceHousingDetails(int id)
+        {
+            var serviceHousing = _serviceHousingService.GetById(id);
+            var serviceMap = _serviceMapService.GetById(serviceHousing.ServiceMapId ?? 0);
+            var serviceInfo = _serviceInfoService.GetById(serviceHousing.ServiceInfoId ?? 0);
+            var servicePhotos = _servicePhotoService.GetByServiceHousingId(serviceHousing.ServiceHousingID);
+            var photoPaths = servicePhotos.Select(photo => photo.FotografYolu).ToList();
+
+            var model = new ServiceHousingAddViewModel
+            {
+                ServiceHousing = serviceHousing,
+                ServiceMap = serviceMap,
+                ServiceInfo = serviceInfo,
+                PhotoPaths = photoPaths,
+                GorselYolu = serviceHousing.Gorsel
+            };
+
+            return View(model);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
         [HttpGet]
         public PartialViewResult SendMessage()
         {
